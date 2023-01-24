ae3c33f9c7f48e9ef95d5f02a0598acfbf8ebbb3
Simon Pflaumer <s.pflaumer@murena.io>
2023-01-24 13:13:17 +0000

    Add License to main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+// GNU GPLv3 License at the bottom of this file
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -94,3 +95,20 @@ int main(int argc, char **argv){
 
     return 0;
 }
+/**
+hyper - small yet fast webserver with multithreading
+    Copyright (C) 2023 Simon Peter Pflaumer aka. spflaumer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
