6a5da9a4c16c12272482534a32f02a6d068c9e56
1nekomata <s.pflaumer@murena.io>
2023-01-24 15:15:14 +0100

    add "useful" comments and tweak the code a bit

---
+++ README.md
@@ -0,0 +1,14 @@
+# hyper
+## An HTTP Server made small yet fast
+At least in theory.
+The aim of this server is to be fast and small.
+But also:
+ - [x] Support HTTP
+    - [x] Multithreaded with pthreads
+    - [ ] Routing 
+ - [ ] Support HTTPS
+ - [ ] Support FTP
+ - [ ] Config with TOML
+ - [ ] Authentication
+
+perhaps even more features will be added in the future.
--- src/main.c
+++ src/main.c
@@ -11,8 +11,8 @@
 
 void *con_handler(void *arg){
     int cfd = *(int*)arg;
-    char *buf = calloc(RECV_BUF_LEN, sizeof(char));
-   
+    char *buf = calloc(RECV_BUF_LEN, sizeof(char)); // ! TODO: create separate buffer for sending
+
     bzero(buf, RECV_BUF_LEN*sizeof(char));
     if(recv(cfd, buf, RECV_BUF_LEN, 0) < 1){
         perror("recv() failed");
@@ -20,7 +20,6 @@ void *con_handler(void *arg){
     }
 
     bzero(buf, RECV_BUF_LEN*sizeof(char));
-    //*(buf + RECV_BUF_LEN - 1) = '\0';
     strcpy(buf, "HTTP/1.1 200 OK\r\n\r\nGood Morning, Mothafucka!");
     if(send(cfd, buf, strlen(buf), 0) < 0){
         perror("send() failed");
@@ -34,47 +33,56 @@ void *con_handler(void *arg){
 }
 
 int main(int argc, char **argv){
+    // Initialize socket and socket file descriptor(sfd)
     int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    // Create and initialize server address
     struct sockaddr_in *addr = calloc(1, sizeof(struct sockaddr_in));
-    addr->sin_family = AF_INET;
-    addr->sin_port = htons(42069);
-    addr->sin_addr.s_addr = INADDR_ANY;
+    addr->sin_family = AF_INET; addr->sin_port = htons(42069); addr->sin_addr.s_addr = INADDR_ANY;
 
+    // bind socket to address specified in addr struct
     if(bind(sfd, (struct sockaddr*)addr, sizeof(struct sockaddr_in)) < 0){
         perror("bind() failed");
         exit(EXIT_FAILURE);
     }
 
+    // listen to incoming connections
     if(listen(sfd, 100) < 0){
         perror("listen() failed");
         exit(EXIT_FAILURE);
     }
 
+    // client addresses will be stored here
     struct sockaddr_in *client = calloc(1, sizeof(struct sockaddr_in));
     socklen_t client_addr_len = sizeof(struct sockaddr_in);
-    int cfd = 0;
 
+    // handle connections in a loop
     while(1){
+        // set client to 0
         bzero(client, sizeof(struct sockaddr_in));
-        cfd = accept(sfd, (struct sockaddr*)client, &client_addr_len);
+        // accept connection from client:
+        //      client address is stored within client
+        //      cfd holds the file descriptor
+        int cfd = accept(sfd, (struct sockaddr*)client, &client_addr_len);
         if(cfd < 0){
             perror("accept() failed");
             exit(EXIT_FAILURE);
         }
 
+        // create a separate thread for every connection
         pthread_t thread;
         if(pthread_create(&thread, NULL, con_handler, &cfd) != 0){
             perror("pthread_create() failed:");
             exit(EXIT_FAILURE);
         }
-
+        // detach thread from main process
         if(pthread_detach(thread) != 0){
             perror("pthread_detahc() failed");
             exit(EXIT_FAILURE);
         }
 
+        // quit once 'q' or 'Q' is received
         char c = fgetc(stdin);
-        if (c == 'q') {
+        if (c == 'q' || c == 'Q') {
             break;
         }
     }
